e692eeabb61724e635fff594e5ce09a1425d6019
adarsh0707-kumar <adarshku123456789@gmail.com>
2025-02-06 04:58:18 +0530

    Dashboard

--- src/Components/Dashboard/Dash/Dashboard.jsx
+++ src/Components/Dashboard/Dash/Dashboard.jsx
@@ -5,14 +5,21 @@ const Dashboard = () => {
   return (
     <div
       className='Dashboard'>
-      
+
       <div
         className="Dashboard__nav">
 
-        <div className="Dashboard__nav__profile">
+        <div
+          className="Dashboard__nav__profile">
+          <img
+            className='Dashboard__nav__profile__img'
+            src={localStorage.getItem('logoUrl')}
+            alt='logo' />
+
+          <h2>{localStorage.getItem('channelName')}</h2>
 
         </div>
-        
+
       </div>
 
       <div
